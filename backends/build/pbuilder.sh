6caa288b6eddd64110f45bd794297e5a93e1967a
raphael <raphael@dbf15aec-6b38-4e42-8090-e3bef7eebefd>
2008-02-23 02:43:38 +0000

    Look for the right basetgz in several places, such as defined by $PBUILDER_$distro, $PBUILDER_CACHE/$distro.tgz and similar
    git-svn-id: https://neo.yahostme.com/svn/DeBaBaReTools/trunk@44 dbf15aec-6b38-4e42-8090-e3bef7eebefd

--- backends/build/pbuilder.sh
+++ backends/build/pbuilder.sh
@@ -22,12 +22,15 @@ setDefault "PBUILDER_CACHE" "/var/cache/pbuilder"
 
 #buildPackage "$dscURI" "$BUILD_TYPE" "$workingDir" "$distro" ["$maintainer"]
 buildPackage() {
-	if [ "$UPDATE_ENVIRONMENT" -gt 0 ]; then
-		updateTGZ
-	fi
 
 	local dscURI="${1:-}" buildType="${2:-}" workingDir="${3:-}" distro="${4:-}" maintainer="${5:-}"
-	local pbuilderopts es logFile
+	local pbuilderopts es logFile basetgz
+
+	basetgz="$(pbuilderFindBasetgz "$distro")" || return
+
+	if [ "$UPDATE_ENVIRONMENT" -gt 0 ]; then
+		updateTGZ "$basetgz"
+	fi
 
 	case $buildType in 
 		binary-arch)
@@ -50,10 +53,11 @@ buildPackage() {
 	Say "\t\tStarting build process"
 
 	if ! $GAINROOT pbuilder --build $pbuilderopts --buildresult "$workingDir" \
-		--basetgz "${PBUILDER_CACHE}/$distro.tgz" "$workingDir/$dscURI" \
-		&> "$logFile"; then
+		--basetgz "$basetgz" "$workingDir/$dscURI" \
+		--logfile "$logFile"
+		&> /dev/null; then
 			es=$?
-			Say "Build failed, cat "$logFile" for more information"
+			Say "\t\tBuild failed, cat "$logFile" for more information"
 			return $es
 	fi
 }
@@ -61,19 +65,37 @@ buildPackage() {
 updateTGZ() {
 	local file output es
 
-	if [ ! -z "${1:-}" ] && [ -f "${PBUILDER_CACHE}/${1:-}.tgz" ]; then
-		file="${1:-}"
-	elif [ -f "${PBUILDER_CACHE}/${DISTRO}.tgz" ]; then
-		file="$DISTRO"
-	else
+	file="$(pbuilderFindBasetgz "${1:-}")"
+
+	if [ ! -f "$file" ]; then
 		Say "err, what basetgz do you want me to update?"
 		return 1
 	fi
-	Say "\t\tExecuting $GAINROOT pbuilder --update --basetgz ${PBUILDER_CACHE}/$file.tgz"
+	Say "\t\tExecuting $GAINROOT pbuilder --update --basetgz $file"
 
-	if ! output="`$GAINROOT pbuilder --update --basetgz "${PBUILDER_CACHE}/$file.tgz" 2>&1`"; then
+	if ! output="`$GAINROOT pbuilder --update --basetgz "$file" 2>&1`"; then
 		es=$?
 		Say "$output"
 		return $es
 	fi
+}
+
+pbuilderFindBasetgz() {
+	local file
+
+	if [ ! -z "${1:-}" ]; then
+        	if  [ -f "${1:-}" ]; then
+			file="${1:-}"
+		elif [ -f "$(eval "echo \$PBUILDER_${1:-}")" ]; then
+			file="$(eval "echo \$PBUILDER_$f")"
+		elif [ -f "${PBUILDER_CACHE}/${1:-}.tgz" ]; then
+			file="${PBUILDER_CACHE}/${1:-}.tgz"
+		fi
+	fi
+
+	if [ -z "$file" ] && [ -f "${PBUILDER_CACHE}/${DISTRO}.tgz" ]; then
+		file="${PBUILDER_CACHE}/${DISTRO}.tgz"
+	fi
+
+	[ -f "$file" ] && echo "$file" || return
 }
